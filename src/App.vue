9c17cecd3595e1dbf73acc2448be0407dd4e6fe1
Edwin Rivera <gaedwin92@gmail.com>
2022-12-17 10:56:05 -0600

    Comenzando proyecto de portafolio

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Menu from "./components/Menu.vue"
+import VistaMain from './components/VistaMain.vue'
+export default {
+  components: { Menu, VistaMain },
+}
+</script>
+ 
+
+<template>
+  <Menu></Menu>
+  <vista-main></vista-main>
+</template>
+
+<style scoped>
+
+</style>
