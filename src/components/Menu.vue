0a42762543c07bdcacf4f8df145f3cce74aaf6ef
Edwin Rivera <gaedwin92@gmail.com>
2022-12-22 19:58:31 -0600

    Diseño de sidebar terminado, comenzando con las rutas

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -4,71 +4,66 @@
 
         <div class="overflow-y-auto py-4 px-3 bg-gray-50 rounded mysidebar">
             <ul class="space-y-2">
-                <div class="dfoto">
-                    <img class="foto" src="../assets/eyr.jpg" alt="Foto" srcset="">
+                <div class="headerSidebar">
+                    <div class="dfoto">
+                        <img class="foto" src="../assets/eyr.jpg" alt="Foto" srcset="">
+                    </div>
+                    <h1 class="tituloNombre">Ronald Edwin Villalobos Rivera</h1>
+                    <hr class="hr">
+                    <h2 class="tituloCarrera">Ing. Desarrollo de Software</h2>
+                    <br>
                 </div>
-                <h1 class="tituloNombre">Ronald Edwin Villalobos Rivera</h1>
-                <hr class="hr">
-                <h2 class="tituloCarrera">Ing. Desarrollo de Software</h2>
+                <div class="bodySidebar">
+                    <li>
+                        <a href="#"
+                            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                            <svg aria-hidden="true"
+                                class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" id="Layer_1" enable-background="new 0 0 512 512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><g><path d="m460.6 2h-338.7c-27.2 0-49.3 22.2-49.3 49.4v409.2c0 11.7-9.5 21.2-21.2 21.2s-21.2-9.5-21.2-21.2v-220.3c0-7.8-6.3-14.1-14.1-14.1s-14.1 6.3-14.1 14.1v220.3c0 27.2 22.2 49.4 49.4 49.4h409.2c27.2 0 49.4-22.2 49.4-49.4v-409.2c0-27.2-22.2-49.4-49.4-49.4zm21.2 458.6c0 11.7-9.5 21.2-21.2 21.2h-364.6c3.1-6.4 4.8-13.6 4.8-21.2v-409.2c0-11.7 9.5-21.2 21.2-21.2h338.7c11.7 0 21.2 9.5 21.2 21.2v409.2z" fill="#194f82"/><path d="m30.2 460.6c0 11.7 9.5 21.2 21.2 21.2s21.2-9.5 21.2-21.2v-220.4h-42.4z" fill="#56ace0"/><path d="m460.6 30.2h-338.7c-11.7 0-21.2 9.5-21.2 21.2v409.2c0 7.6-1.7 14.7-4.8 21.2h364.6c11.7 0 21.2-9.5 21.2-21.2v-409.2c.1-11.7-9.4-21.2-21.1-21.2z" fill="#fff"/><g fill="#194f82"><path d="m439.4 72.6h-294.8c-7.8 0-14.1 6.3-14.1 14.1s6.3 14.1 14.1 14.1h294.9c7.8 0 14.1-6.3 14.1-14.1s-6.4-14.1-14.2-14.1z"/><path d="m439.4 143.1h-294.8c-7.8 0-14.1 6.3-14.1 14.1s6.3 14.1 14.1 14.1h294.9c7.8 0 14.1-6.3 14.1-14.1s-6.4-14.1-14.2-14.1z"/><path d="m144.6 241.9h105.4c7.8 0 14.1-6.3 14.1-14.1s-6.3-14.1-14.1-14.1h-105.4c-7.8 0-14.1 6.3-14.1 14.1s6.3 14.1 14.1 14.1z"/><path d="m144.6 312.4h105.4c7.8 0 14.1-6.3 14.1-14.1s-6.3-14.1-14.1-14.1h-105.4c-7.8 0-14.1 6.3-14.1 14.1s6.3 14.1 14.1 14.1z"/><path d="m267.5 354.8h-123c-7.8 0-14.1 6.3-14.1 14.1s6.3 14.1 14.1 14.1h123c7.8 0 14.1-6.3 14.1-14.1s-6.3-14.1-14.1-14.1z"/><path d="m267.5 425.3h-123c-7.8 0-14.1 6.3-14.1 14.1s6.3 14.1 14.1 14.1h123c7.8 0 14.1-6.3 14.1-14.1.1-7.7-6.3-14.1-14.1-14.1z"/></g><path d="m342.5 364.7v56.4l16.1-12.2c5-3.8 12-3.8 17 0l16.1 12.2v-56.4c-7.8 2.3-16.1 3.6-24.6 3.6-8.6 0-16.8-1.3-24.6-3.6z" fill="#ffc10d"/><path d="m367.1 223.7c-32.1 0-58.2 26.1-58.2 58.2s26.1 58.2 58.2 58.2 58.2-26.1 58.2-58.2-26.1-58.2-58.2-58.2z" fill="#ffc10d"/><path d="m367.1 195.4c-47.7 0-86.5 38.8-86.5 86.5 0 27.8 13.2 52.4 33.6 68.3v99.3c.9 13.6 14.1 17.3 22.6 11.3l30.2-22.8 30.2 22.8c9.7 7.7 23.4-1.7 22.6-11.3v-99.3c20.4-15.8 33.6-40.5 33.6-68.3.2-47.7-38.6-86.5-86.3-86.5zm24.6 225.7-16.1-12.1c-5-3.8-12-3.8-17 0l-16.1 12.2v-56.4c7.8 2.3 16.1 3.6 24.6 3.6 8.6 0 16.8-1.3 24.6-3.6zm-24.6-81c-32.1 0-58.2-26.1-58.2-58.2s26.1-58.2 58.2-58.2 58.2 26.1 58.2 58.2-26.1 58.2-58.2 58.2z" fill="#194f82"/></g></svg>
+                            <span class="ml-3">Sobre mi</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                            <!-- <svg aria-hidden="true"
+                                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z">
+                                </path>
+                            </svg> -->
+                            <!-- icon666.com - MILLIONS vector ICONS FREE -->
+                            <svg aria-hidden="true"
+                                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" id="Flat" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg"><path d="m16.5714 25.4286h12a0 0 0 0 1 0 0v33.1428a6 6 0 0 1 -6 6 6 6 0 0 1 -6-6v-33.1428a0 0 0 0 1 0 0z" fill="#efefff" transform="matrix(0 1 -1 0 67.571 22.429)"/><circle cx="42.143" cy="45" fill="#c3c3dc" r="6"/><path d="m48.1426 45.0054a3.9416 3.9416 0 0 0 -3.9562-4.095h-6.4253a5.9717 5.9717 0 0 0 -1.6182 4.0947z" fill="#efefff"/><path d="m36.1429 45a5.9951 5.9951 0 0 0 .2782 1.8072h6.9715a2.0966 2.0966 0 0 0 2.0967-2.0966h-9.3318c-.0046.0965-.0146.1917-.0146.2894z" fill="#efefff"/><path d="m44.0479 40.91h-6.2868a5.9858 5.9858 0 0 0 -1.34 5.8968h6.9715a2.6208 2.6208 0 0 1 -2.6207-2.6208 3.2759 3.2759 0 0 1 3.276-3.276z" fill="#ddddf0"/><path d="m46.4074 14h-30.8148v18a60.4224 60.4224 0 0 0 15.4074 2 60.41 60.41 0 0 0 15.4074-2z" fill="#373c4b"/><path d="m57 14-26 11-26-11 26-11z" fill="#465064"/><path d="m57 13h-26a1 1 0 0 0 0 2h25v16a1 1 0 0 0 2 0v-17a1 1 0 0 0 -1-1z" fill="#ffe300"/><path d="m57 34a3.26 3.26 0 0 0 -3.2491 2.99l-.7509 9.01h8l-.7509-9.01a3.26 3.26 0 0 0 -3.2491-2.99z" fill="#ffe300"/><circle cx="57" cy="32" fill="#fff089" r="2"/><path d="m19.008 45 5.46 1.987-5.465 15.013-1.984-3.041-3.475 1.054z" fill="#ff3d46"/><path d="m26.992 45-5.46 1.987 5.465 15.013 1.984-3.041 3.475 1.054z" fill="#ff3d46"/><circle cx="23" cy="45" fill="#ff6e81" r="8"/><circle cx="23" cy="45" fill="#ffe300" r="6"/></svg>
+                            <span class="flex-1 ml-3 whitespace-nowrap">Estudios</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                            <svg aria-hidden="true"
+                                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><g id="open_Folder" data-name="open Folder"><path d="m19.8 12.4-3-4a.9993.9993 0 0 0 -.8-.4h-12a1 1 0 0 0 -1 1v14a1 1 0 0 0 1.8193.5732l6.7012-9.5732h7.4795a1 1 0 0 0 .8-1.6z" fill="#4294ff"/><path d="m10.17 15.92-5.35 7.65a.9845.9845 0 0 1 -.82.43.7887.7887 0 0 1 -.3-.05.9871.9871 0 0 1 -.7-.95v-14a.99.99 0 0 1 .82-.98 19.1394 19.1394 0 0 0 6.35 7.9z" fill="#2965ed"/><path d="m28.8867 12.5381a.9983.9983 0 0 0 -.8867-.5381h-17a1 1 0 0 0 -.8193.4268l-7 10a1 1 0 0 0 .8193 1.5732h17a1 1 0 0 0 .8193-.4268l7-10a.9979.9979 0 0 0 .0674-1.0351z" fill="#e6ecff"/><path d="m24.12 20.29-2.3 3.28a.9966.9966 0 0 1 -.82.43h-17a1.02 1.02 0 0 1 -.89-.54 1.0035 1.0035 0 0 1 .07-1.03l5.43-7.76c3.28 2.93 8.18 5.24 15.51 5.62z" fill="#b7c6e2"/></g></svg>
+                            <span class="flex-1 ml-3 whitespace-nowrap">Portafolio</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+                            <svg aria-hidden="true"
+                                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" id="Layer_1" enable-background="new 0 0 64 64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg"><g>
+                                <path d="m45.848 23.235c-.31-.748-1.039-1.235-1.848-1.235h-4v-20c0-1.104-.896-2-2-2h-12c-1.105 0-2 .896-2 2v20h-4c-.809 0-1.538.487-1.848 1.235-.309.747-.139 1.607.434 2.179l12 12c.39.391.902.586 1.414.586s1.024-.195 1.414-.586l12-12c.572-.572.743-1.432.434-2.179z" fill="#3d9ae2"/><path d="m58 40h-14c-.53 0-1.039.211-1.414.586l-3.415 3.414h-14.342l-3.415-3.414c-.375-.375-.884-.586-1.414-.586h-14c-1.104 0-2 .896-2 2v12h56v-12c0-1.104-.896-2-2-2z" fill="#c4c8f5"/><path d="m4 54v6c0 2.209 1.791 4 4 4h48c2.209 0 4-1.791 4-4v-6z" fill="#3d9ae2"/></g>
+                            </svg>
+                            <span class="ml-3">Descargar PDF</span>
+                        </a>
+                    </li>                    
+                </div>
                 <br>
-                <br>
-                <li>
-                    <a href="#"
-                        class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                        <svg aria-hidden="true"
-                            class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                            fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                            <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                        </svg>
-                        <span class="ml-3">Sobre mi</span>
-                        <font-awesome-icon icon="fa-solid fa-user-secret" />
-                        <font-awesome-icon icon="fa-solid fa-card" />
-                    </a>
-                </li>
-                <li>
-                    <a href="#"
-                        class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                        <svg aria-hidden="true"
-                            class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                            fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z">
-                            </path>
-                        </svg>
-                        <span class="flex-1 ml-3 whitespace-nowrap">Estudios</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#"
-                        class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                        <svg aria-hidden="true"
-                            class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                            fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z">
-                            </path>
-                            <path
-                                d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z">
-                            </path>
-                        </svg>
-                        <span class="flex-1 ml-3 whitespace-nowrap">Portafolio</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#"
-                        class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
-                        <svg aria-hidden="true"
-                            class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                            fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
-                            <path fill-rule="evenodd"
-                                d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-                                clip-rule="evenodd"></path>
-                        </svg>
-                        <span class="ml-3">Descargar PDF</span>
-                    </a>
-                </li>
+                <div class="footerSidebar">
+                    <h3>CONTACTOS</h3>
+                    <hr class="hr">
+                    <a href="mailto:gaedwin92@gmail.com">gaedwin92@gmail.com</a>
+                    <a href="tel:+50378829786">+503 7882 9786</a>
+                </div>
             </ul>
         </div>
     </aside>
@@ -84,7 +79,7 @@ export default {
 .sidebar {
     height: 100vh;
     margin: 0;
-    max-width: 25%;
+    max-width: 20%;
     padding: 0;
     width: 20%;
 }
@@ -98,8 +93,8 @@ export default {
     border: 2px #fff solid;
     border-radius: 25px;
     margin: 0 auto;
-    height: 200px;
-    width: 60%;
+    height: 180px;
+    width: 55%;
 }
 .hr{
     border: 2px solid #fff; 
@@ -117,5 +112,15 @@ export default {
 .tituloCarrera{
     color: #E4C109;
     text-align: center;
+}
+.footerSidebar {
+    color: #E4C109;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    margin-bottom: 0;
+}
+.footerSidebar a{
+    color: #E4C109;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-library.add(faUserSecret)
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).mount('#app')
